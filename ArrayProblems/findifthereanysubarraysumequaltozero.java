a6efd5759b5f3b5418b956a6f673b5e67f09fa4d
hars123 <harshitshukla4513@gmail.com>
2024-06-17 21:09:09 +0530

    Add

--- ArrayProblems/findifthereanysubarraysumequaltozero.java
+++ ArrayProblems/findifthereanysubarraysumequaltozero.java
@@ -21,8 +21,8 @@ public class findifthereanysubarraysumequaltozero {
 
  public static void main(String[] args) {
   int arr[] = { 4, 2, -3, 1, 6 };
-
   System.out.println(SUBARRAYEXISTS(arr));
+  
 
  }
 }
