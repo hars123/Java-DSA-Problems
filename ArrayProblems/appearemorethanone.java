23d83f6d28ed9897800a5425338af99086fda3ed
hars123 <harshitshukla4513@gmail.com>
2024-07-09 21:04:32 +0530

    Add

--- ArrayProblems/appearemorethanone.java
+++ ArrayProblems/appearemorethanone.java
@@ -1,4 +1,20 @@
+import java.util.HashMap;
+
 public class appearemorethanone {
+
+ static void morethanNK(int a[], int n, int k) {
+  int x = n / k;
+  HashMap<Integer, Integer> y = new HashMap<>();
+  for (int i = 0; i < n; i++) {
+   if (!y.containsKey(a[i]))
+    y.put(a[i], 1);
+   else {
+    int count = y.get(a[i]);
+    y.put(a[i], count + 1);
+   }
+  }
+
+ }
 
  public static void main(String[] args) {
 
