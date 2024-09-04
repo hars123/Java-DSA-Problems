08b364d00c542b8f840cf1fdb80ad0f854cbd891
Harshit Shukla <harshitshukla4513@gmail.com>
2024-09-04 22:54:48 +0530

    ADd

---
+++ Stringproblems/Validpalindromeornot.java
@@ -0,0 +1,41 @@
+/**
+ * Validpalindromeornot
+ */
+public class Validpalindromeornot {
+
+    public static  boolean Palindrome(String s){
+
+        if(s.isEmpty()){
+            return false;
+        }
+        int start = 0, last = s.length()-1;
+        while (start<= last) {
+            char currfirst = s.charAt(start);
+            char currlast = s.charAt(last);
+
+            if(!Character.isLetter(currfirst)){
+                start++;
+            }else if(!Character.isLetter(currlast)){
+                last--;
+            }else{
+                if(Character.toLowerCase(currfirst) != Character.toLowerCase(currlast)){
+                    return false;
+                }
+                start++;
+                last--;
+                
+            }
+         
+        }
+        return true;
+
+
+    }
+
+    
+    public static void main(String[] args) {
+
+       System.out.println(Palindrome("amanaplanacanalpanama"));
+        
+    }
+}
