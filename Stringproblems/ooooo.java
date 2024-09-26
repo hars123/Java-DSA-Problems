86c9cfefd56b7ec56f64dfcc621a10d0292de54c
Harshit Shukla <harshitshukla4513@gmail.com>
2024-09-26 09:30:57 +0530

    addd

---
+++ Stringproblems/ooooo.java
@@ -0,0 +1,12 @@
+/**
+ * ooooo
+ */
+public class ooooo {
+
+    public static void main(String[] args) {
+        int a= 8, b=7, c, d;
+        c=(a&b);
+        System.out.println(c);
+        
+    }
+}
