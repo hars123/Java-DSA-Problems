7fd327fbf72edcbdebb1f4e56179ddf43af84a3f
Harshit Shukla <harshitshukla4513@gmail.com>
2024-10-01 19:09:30 +0530

    aad

--- Stringproblems/Duplicatecharacter.java
+++ Stringproblems/Duplicatecharacter.java
@@ -3,32 +3,25 @@ import java.util.HashSet;
 public class Duplicatecharacter {
 
     public static void main(String[] args) {
-        // String A = "HARSHIT";
-        // HashSet seen = new HashSet<>();
-        // for(int i = 0; i<A.length();i++){
-        //    if(!seen.add(A.charAt(i))){
-        //     System.out.println(A.charAt(i));
-        //    }
-        // }
+     
 
 
         // String s = "HARSHIT";
         // HashSet seen = new HashSet<>();
-        // for(int i = 0;i<s.length();i++){
-        //     if(!seen.add(s.charAt(i))){
+
+        // for(int i = 0; i<s.length();i++){
+        //    if(!seen.add(s.charAt(i))){
         //         System.out.println(s.charAt(i));
-        //     }
+
+        //    }
         // }
 
-
-        String s = "HARSHIT";
+        String s = "HarsHit";
         HashSet seen = new HashSet<>();
 
-        for(int i = 0; i<s.length();i++){
-           if(!seen.add(s.charAt(i))){
-                System.out.println(s.charAt(i));
-
-           }
+        for(int i = 0;i<s.length();i++){
+            if(!seen.add(s.charAt(i)))
+            System.out.println(s.charAt(i));
         }
         
         
