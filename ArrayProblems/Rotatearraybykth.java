77db6c87606aa15be02b32e09077ac658f0a67e2
hars123 <harshitshukla4513@gmail.com>
2024-11-10 19:30:51 +0530

    ad

--- ArrayProblems/Rotatearraybykth.java
+++ ArrayProblems/Rotatearraybykth.java
@@ -1,30 +1,50 @@
 public class Rotatearraybykth {
      
     public static void reverse(int nums[], int start, int end){
-        while(start < end){
+        // while(start < end){
+        //     int temp = nums[start];
+        //     nums[start] = nums[end];
+        //     nums[end] = temp;
+        //     start++;
+        //     end--;  
+        // }
+
+
+
+         while (start < end) {
             int temp = nums[start];
             nums[start] = nums[end];
             nums[end] = temp;
             start++;
-            end--;  
-        }
+            end--;
+            
+         }
     }
-    public  static void rotate(int[] nums, int k) {
-        k %= nums.length;
-        reverse(nums, 0 , nums.length-1);
-        reverse(nums, 0, k-1);
-        reverse(nums,k, nums.length-1);
-    }
+    // public  static void rotate(int[] nums, int k) {
+    //     k %= nums.length;
+    //     reverse(nums, 0 , nums.length-1);
+    //     reverse(nums, 0, k-1);
+    //     reverse(nums,k, nums.length-1);
+    // }
 
     public static void printarray(int arr[]){
         for(int i = 0; i<arr.length;i++){
             System.out.print(arr[i] + " ");
         }
     }
+     
 
+
+    public  static void rotate(int nums[] , int k  ){
+        k %= nums.length;
+        reverse(nums, 0, nums.length-1);
+        reverse(nums, 0, k-1);
+        reverse(nums, k, nums.length-1);
+
+    }
     public static void main(String[] args) {
         int arr[] = {1,2,3};
-        rotate(arr, 1);
+        rotate(arr, 3);
         printarray(arr );
 
     
