1c6f78613a8d0905592fa27853e4e33fb2b4c05f
hars123 <harshitshukla4513@gmail.com>
2024-11-19 00:15:18 +0530

    add:

--- ArrayProblems/MergeIntervalsinjavadsaproblembylovebabbar.java
+++ ArrayProblems/MergeIntervalsinjavadsaproblembylovebabbar.java
@@ -1,4 +1,26 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class MergeIntervalsinjavadsaproblembylovebabbar {
+    public static int[][] Mergeintervals(int intervals[][]){
+        if(intervals.length <= 1) return intervals;
+        Arrays.sort(intervals, Comparator.comparingInt(i->i[0]));
+        ArrayList<int[]> result = new ArrayList<>();
+        int newinterval[] = intervals[0];
+        result.add(newinterval);
+
+        for(int interval[] : intervals){
+            if(interval[0] <= newinterval[1])
+            newinterval[1] = Math.max(newinterval[1],interval[1]);
+            else{
+                newinterval = interval;
+                result.add(newinterval);
+            }
+        }
+        return result.toArray(new int[result.size()][]);
+
+    }
 
     public static void main(String[] args) {
         
