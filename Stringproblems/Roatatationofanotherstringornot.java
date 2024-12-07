57d09bb8668534b2ef2546cb1de7fe9d92dac11e
hars123 <harshitshukla4513@gmail.com>
2024-12-07 20:58:48 +0530

    @add@

--- Stringproblems/Roatatationofanotherstringornot.java
+++ Stringproblems/Roatatationofanotherstringornot.java
@@ -4,10 +4,24 @@ public class Roatatationofanotherstringornot {
 
     public static void main(String[] args) {
         String  S1 = "ABCD";
-        String  S2 = "CDBA";
+        String  S2 = "DCBA";
         if(S1.length() != S2.length()){
             System.out.println(" Not A Rotation ");
+            System.exit(0);
         }  
+
+        String S3 = S1+S1;
+        if(S3.indexOf(S2) != -1){
+            System.out.println("String is Roatation");
+
+        }else{
+            System.out.println("Not a Rotation");
+
+        }
+
+
+
+
     }
-    
+
 }
