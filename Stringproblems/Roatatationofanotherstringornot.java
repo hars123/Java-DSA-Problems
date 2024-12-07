e2d719627375cf10ad7ec098648a28473791e08d
hars123 <harshitshukla4513@gmail.com>
2024-12-07 20:46:31 +0530

    add

--- Stringproblems/Roatatationofanotherstringornot.java
+++ Stringproblems/Roatatationofanotherstringornot.java
@@ -3,6 +3,10 @@
 public class Roatatationofanotherstringornot {
 
     public static void main(String[] args) {
-        
+        String  S1 = "ABCD";
+        String  S2 = "CDBA";
+        if(S1.length() != S2.length()){
+            System.out.println(" Not A Rotation ");
+        }  
     }
 }
