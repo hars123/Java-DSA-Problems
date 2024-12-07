05887e97a55f767d1328acee4bf083ca4030ab90
hars123 <harshitshukla4513@gmail.com>
2024-12-07 21:36:45 +0530

    Add

--- Stringproblems/Countandsayproblem.java
+++ Stringproblems/Countandsayproblem.java
@@ -1,7 +1,37 @@
 // Count and say Problem 
 public class Countandsayproblem {
 
+    public static  String countAndSay(int n) {
+        int i = 1;
+        StringBuilder sb = new StringBuilder("1");
+        while(i<n){
+            sb = convert(sb);
+            i++;
+            // sb = s;
+        }
+        return sb.toString();
+    }
+    public static StringBuilder convert(StringBuilder sb){
+        StringBuilder s = new StringBuilder();
+        int count = 1;
+        int left = 0;
+        int right = 1;
+        while(left <sb.length()){
+            while(right<sb.length() && sb.charAt(left)==sb.charAt(right)){
+                right++;
+            
+                count++;
+            }
+            s.append(count+""+sb.charAt(left));
+            left = right;
+            right++;
+            count = 1;
+        }
+        return s;
+    }
+
     public static void main(String[] args) {
+        System.out.println(countAndSay(4));
         
     }
 }
