babca8d4719ee64d95d04d3239ffd20bb08cccd0
hars123 <harshitshukla4513@gmail.com>
2024-12-15 17:18:03 +0530

    add

--- Stringproblems/Fiindsubsequenceongivenstring.java
+++ Stringproblems/Fiindsubsequenceongivenstring.java
@@ -1,13 +1,23 @@
 public class Fiindsubsequenceongivenstring {
 
     public static void printsubsequnce(String str , String current , int index){
-        
+        if(index == str.length()){
+            System.out.println(current);
+            return;
 
+          
+
+        }
+        // Include the current character int the subsequence
+        printsubsequnce(str, current + str.charAt(index), index + 1);
+        // exclude the current character form the subsequence 
+        printsubsequnce(str, current, index+1);
     }
 
     
     public static void main(String[] args) {
-        
-        
+        String str = "abc";
+        System.out.println("All subsequence of the string " + str );
+        printsubsequnce(str, "", 0);            
     }
 }
