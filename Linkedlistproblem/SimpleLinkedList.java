44c119743aea22cda1bd280f669b16b63a5258db
hars123 <harshitshukla4513@gmail.com>
2025-01-12 23:14:08 +0530

    add

---
+++ Linkedlistproblem/SimpleLinkedList.java
@@ -0,0 +1,69 @@
+public class SimpleLinkedList {
+
+    public static class Node{
+        int data;
+        Node next;
+
+        Node (int data){
+            this.data = data;
+            
+        }
+    }
+
+    public static class Linkedlist{
+        Node head;
+
+         public void Reverse(){
+            Node previous = null;
+            Node current = head;
+            Node next = null;
+
+            while (current != null) {
+                next = current.next;
+                current.next = previous;
+                previous = current;
+                current  = next;
+                
+            }
+            head = previous;
+         }
+
+         public void PrintList(){
+            Node temp = head;
+            while (temp != null) {
+            System.out.println(temp.data);
+            temp = temp.next;                
+            }
+            System.out.println("null");
+         }
+
+        
+    }
+
+    public static void main(String[] args) {
+        Linkedlist list = new Linkedlist();
+
+        list.head = new Node(4);
+        list.head.next = new Node(5);
+        list.head.next.next = new Node(6);
+        list.PrintList();
+        list.Reverse();
+        list.PrintList();
+        
+
+
+       
+
+
+
+
+
+
+
+
+
+
+        
+        
+    }
+}
