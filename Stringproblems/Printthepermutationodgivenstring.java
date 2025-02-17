acde5a012cf441e324b6e7079834d1726ef6c0ab
hars123 <harshitshukla4513@gmail.com>
2025-02-18 00:36:27 +0530

    add

--- Stringproblems/Printthepermutationodgivenstring.java
+++ Stringproblems/Printthepermutationodgivenstring.java
@@ -6,14 +6,14 @@ public class Printthepermutationodgivenstring {
             System.out.println(current);
             return;
         }
-        // Loop through all the chracter of the string 
+         
 
         for(int i = 0; i< str.length();i++){
             char ch = str.charAt(i);
-            // Extract the character at index i
+            
             String remaining = str.substring(0, i) + str.substring(i+1);
 
-            // Recurse with the current character added to the result
+            
 
             printpermutation(remaining, current + ch);
 
@@ -25,6 +25,7 @@ public class Printthepermutationodgivenstring {
         System.out.println("All permutations of the string \"" + str + "\":");
         printpermutation(str, "");
     }
+    
     
 }
 
