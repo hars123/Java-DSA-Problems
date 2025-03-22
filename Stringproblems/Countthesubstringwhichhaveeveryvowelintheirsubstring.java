994a5fe22da81934e664e673350e37277ce3b8bb
hars123 <harshitshukla4513@gmail.com>
2025-03-22 21:19:07 +0530

    add

--- Stringproblems/Countthesubstringwhichhaveeveryvowelintheirsubstring.java
+++ Stringproblems/Countthesubstringwhichhaveeveryvowelintheirsubstring.java
@@ -1,7 +1,23 @@
-public class Countthesubstringwhichhaveeveryvowelintheirsubstring {
+// import java.util.Arrays;
+// import java.util.HashSet;
+// import java.util.Set;
+
+// public class Countthesubstringwhichhaveeveryvowelintheirsubstring {
     
-    public static void main(String[] args) {
+//     static int countSubstring(String word , int k){
+//         int n = word.length();
+//         Set<Character> vowels = new HashSet<>(Arrays.asList('a','e','i','o','u'));
+//         int count = 0;
+
+//         for(int i = 0; i<n;i++){
+            
+
+//         }
+
+//     }
+
+//     public static void main(String[] args) {
         
-    }
+//     }
     
-}
+// }
