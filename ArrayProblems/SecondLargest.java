cc81b50ef0d0f0a30091479201f12cc3ddd19b72
hars123 <harshitshukla4513@gmail.com>
2025-05-17 15:29:42 +0530

    add

--- ArrayProblems/SecondLargest.java
+++ ArrayProblems/SecondLargest.java
@@ -1,27 +1,90 @@
-public class SecondLargest {
-    public static void main(String[] args) {
-        int[] arr = {10, 20, 4, 45, 99};
-        int largest = arr[0], secondLargest = Integer.MIN_VALUE;
+// // // public class SecondLargest {
+// // //     public static void main(String[] args) {
+// // //         int[] arr = {10, 20, 4, 45, 99};
+// // //         int largest = arr[0], secondLargest = Integer.MIN_VALUE;
 
-        for (int num : arr) {    
-            // First loop to find the largest element
-            if (num > largest) {
-                largest = num;
-            }
-        }
+// // //         for (int num : arr) {    
+            
+// // //             if (num > largest) {
+// // //                 largest = num;
+// // //             }
+// // //         }
 
        
-        for (int num : arr) {    // Second loop to find the second largest element
-            if (num > secondLargest && num < largest) {
-                secondLargest = num;
+// // //         for (int num : arr) {   
+// // //             if (num > secondLargest && num < largest) {
+// // //                 secondLargest = num;
+// // //             }
+// // //         }
+
+       
+// // //         if (secondLargest == Integer.MIN_VALUE) {
+// // //             System.out.println("No second largest element.");
+// // //         } else {
+// // //             System.out.println("Second largest element: " + secondLargest);
+// // //         }
+// // //     }
+// // // }
+
+
+// public class SecondLargest {
+
+//     public static void main(String[] args) {
+//         int arr[] = {1,2,3,4,5};
+//         int largest = arr[0];
+//         int secondlar = 0;
+        
+
+//         for(int num : arr){
+//             if(num > largest){
+//                 largest = num;
+//             }
+//         }
+
+//         for(int num : arr){
+//             if(num > secondlar && num<largest){
+//                 secondlar = num;
+//             }
+//         }
+
+//      if(num == Integer.MIN_VALUE){
+//         System.out.println("No elemenet");
+//      }else{
+//         System.out.println(secondlar);
+//      }
+        
+//     }
+// }
+
+
+
+
+public class SecondLargest {
+
+    public static void findsecondlargestnumber(int arr[]){
+        int largest = arr[0];
+        int secondlargest = 0;
+
+        for(int num :arr){
+            if(num > largest){
+                largest = num;
             }
+
+          
         }
 
-        // Output 
-        if (secondLargest == Integer.MIN_VALUE) {
-            System.out.println("No second largest element.");
-        } else {
-            System.out.println("Second largest element: " + secondLargest);
+        for(int num : arr){
+            if(num > secondlargest && num<largest){
+                secondlargest = num;
+            }
         }
+        System.out.println(secondlargest);
+
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {12,22,3,44,3};
+        findsecondlargestnumber(arr);
+
     }
 }
